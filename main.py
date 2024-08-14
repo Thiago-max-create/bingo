0fb5e9bfea29c8a3ef0d9eb7c83b9a99860e14d5
thiago <1005546@senacminas.edu.br>
2024-08-14 14:06:04 -0300

    Feat: Sortea numeros s/ repetir

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import tkinter as tk
+import random
+import time
+
+numeroSorteado = []
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+root = tk.Tk()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+label_var = tk.StringVar()
+root.title("BINGO")
+root.iconbitmap('./assents/BINGO.ico')
+
+tk.Label(root, text="resultado: ").grid(row=3, column=0)
+tk.Label(root, textvariable=label_var).grid(row=0, column=0)
+
+
+
+button_style = {
+    'font': ('Arial', 11),
+    'bg': '#FF0000',
+    'fg': '#ffffff',
+    'relief': 'raised',
+    'bd': 3,
+    'width': 10,
+    'height': 10
+}
+
+
+def sortear():
+        if len(numeroSorteado) < 75:
+            numero = random.randint(1, 75)
+            while numero in numeroSorteado:
+                numero = random.randint(1, 75)
+            numeroSorteado.append(numero)
+            label_var.set(numeroSorteado)
+        else:
+            label_var.set(text='Fim do jogo')
+
+
+
+
+tk.Button(root, text="SORTEAR",  command=sortear, **button_style).grid(row=1, column=1)
+
+root.mainloop()
+
+
+
